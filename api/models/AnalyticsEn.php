6ff4acb29205b777397d1f0a15e4848bf700fd56
Shkodenko V. Taras <taras.shkodenko@blackbit.de>
2022-12-27 15:56:51 +0200

    Added common and api analytics models

---
+++ api/models/AnalyticsEn.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @author    Taras Shkodenko <taras@shkodenko.com>
+ */
+
+namespace api\models;
+
+
+use Yii;
+use common\models\AnalyticsEn as CommonAnalyticsEn;
+
+
+/**
+ * This is the model class for table "{{%analytics_en}}".
+ *
+ * @property int $id
+ * @property int|null $image_id
+ * @property int|null $user_id
+ * @property int|null $route_id
+ * @property int|null $parent_id
+ * @property string $h1
+ * @property int $deleted
+ * @property string $published
+ * @property string $short_text
+ * @property string|null $subpath
+ * @property string|null $created
+ * @property string|null $updated
+ * @property string $attr (DC2Type:json_array)
+ * @property int $status
+ * @property int $class
+ * @property float|null $rating
+ * @property int|null $sort1
+ * @property int|null $past2
+ * @property string|null $picture_url
+ * @property string|null $category_title
+ * @property int|null $page_views
+ */
+class AnalyticsEn extends CommonAnalyticsEn
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [];
+    }
+}
