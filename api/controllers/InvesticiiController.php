6b7cdd7c40be52ea1886b08805ce7ae33bd4ce04
Taras Shkodenko <shkodenko.t@novadigital.com>
2024-08-23 13:21:15 +0300

    Added InvestsRuSearch.php, improved InvesticiiController.php added search ru model feature

--- api/controllers/InvesticiiController.php
+++ api/controllers/InvesticiiController.php
@@ -8,9 +8,8 @@ declare(strict_types=1);
 
 namespace api\controllers;
 
-
 use yii\rest\ActiveController;
-
+use api\models\InvestsRuSearch;
 
 /**
  * Class InvesticiiController
@@ -20,6 +19,19 @@ use yii\rest\ActiveController;
 class InvesticiiController extends ActiveController
 {
     public $modelClass = 'api\models\InvestsRu';
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        // Override the `index` action to use the search model
+        $actions['index']['prepareDataProvider'] = function ($action) {
+            $searchModel = new InvestsRuSearch();
+            return $searchModel->search(\Yii::$app->request->queryParams);
+        };
+
+        return $actions;
+    }
 
     public function fields()
     {
---
+++ api/models/InvestsRuSearch.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author    Taras Shkodenko <taras@shkodenko.com>
+ * @copyright Shkodenko V. Taras, https://www.shkodenko.com/
+ */
+
+namespace api\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+class InvestsRuSearch extends InvestsRu
+{
+    public function rules()
+    {
+        return [
+            [['parent_id', 'route_id'], 'integer'],
+            [['id', 'h1', 'published', 'short_text', 'subpath', 'created'], 'safe'],
+        ];
+    }
+
+    public function search($params)
+    {
+        $query = InvestsRu::find();
+
+        // Create ActiveDataProvider instance with the query
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        // Load the search parameters
+        $this->load($params);
+
+        // If validation fails, return the data provider without applying any filters
+        if (!$this->validate()) {
+            return $dataProvider;
+        }
+
+        // Apply filtering conditions here
+        $query->andFilterWhere(['parent_id' => $this->parent_id])
+            ->andFilterWhere(['route_id' => $this->route_id]);
+
+        // Add other conditions if necessary
+        $query->andFilterWhere(['like', 'h1', $this->h1])
+            ->andFilterWhere(['like', 'short_text', $this->short_text]);
+
+        return $dataProvider;
+    }
+}
