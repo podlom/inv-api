4a4f35f3a94e1708f4dca08421e4a8d53c4a728d
Taras Shkodenko <taras.shkodenko@gmail.com>
2025-06-19 11:52:24 +0300

    Added common DigestReviews models for uk, en, ru

---
+++ common/models/DigestReviewsRu.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author    Taras Shkodenko <taras.shkodenko@gmail.com>
+ * @copyright Shkodenko V. Taras, https://www.shkodenko.com/
+ */
+
+namespace common\models;
+
+use Yii;
+
+
+/**
+ * This is the model class for table "{{%digest_reviews_ru}}".
+ *
+ * @property int $id
+ * @property string $image_url
+ * @property string $category
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $company
+ * @property string $job
+ * @property string $rtext
+ * @property string $facebook_url
+ * @property string $linkedin_url
+ * @property string|null $published
+ * @property string|null $created
+ * @property string|null $updated
+ * @property int $status
+ * @property string $lang
+ */
+class DigestReviewsRu extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%digest_reviews_ru}}';
+    }
+}
